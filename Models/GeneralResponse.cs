013681e7c57e9e4012468cd913b478c9ec17c3f6
iqnefo1310 <iqbalnafis13@gmail.com>
2025-01-10 14:25:18 +0700

    Save untuk Merge

---
+++ Models/GeneralResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace PERT_2.Models
+{
+    public class GeneralResponse
+    {
+        public string StatusCode { get; set; }
+        public string StatusDesc { get; set; }
+        public object Data { get; set; }
+    }
+}
